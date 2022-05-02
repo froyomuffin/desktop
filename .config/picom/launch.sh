d864ce87be3555955399713bf8480e1efdf69987
Tom Wang <me@tom.af>
2022-05-01 22:36:10 -0400

    Initial

---
+++ .config/picom/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Terminate already running picom instances
+killall -q picom
+
+# Launch picom, using default config location ~/.config/picom/picom.conf
+picom 2>&1 | tee -a /tmp/picom.log & disown
+
+echo "Picom launched..."
