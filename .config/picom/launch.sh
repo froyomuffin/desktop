39f44323b2f11d4e5faf4d02c006021a454eb904
Tom Wang <me@tom.af>
2022-11-27 16:14:49 -0500

    Sync

--- .config/picom/launch.sh
+++ .config/picom/launch.sh
@@ -4,6 +4,6 @@
 killall -q picom
 
 # Launch picom, using default config location ~/.config/picom/picom.conf
-picom --experimental-backend 2>&1 | tee -a /tmp/picom.log & disown
+picom 2>&1 | tee -a /tmp/picom.log & disown
 
 echo "Picom launched..."
